54162db57290bb98c5edd460e8fc6c710e9a8708
idin rohidin <39847803+idgsxr@users.noreply.github.com>
2018-09-01 12:20:47 +0700

    Update page.js

--- page.js
+++ page.js
@@ -38,14 +38,15 @@ $(function(){
 });
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("navbar").style.top = "-50";
-    } else {
-        document.getElementById("navbar").style.top = "0px";
-    }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 }
 
 $(document).ready(function(){
