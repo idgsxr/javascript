217efddf5e69853dd956f1f11406a3c486684217
idin rohidin <39847803+idgsxr@users.noreply.github.com>
2018-09-01 10:22:26 +0700

    Update page.js

--- page.js
+++ page.js
@@ -42,9 +42,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("navbar").style.top = "0";
+        document.getElementById("navbar").style.top = "-50";
     } else {
-        document.getElementById("navbar").style.top = "50px";
+        document.getElementById("navbar").style.top = "0px";
     }
 }
 
